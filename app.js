121226792473ecd19599e0d872186ca972790b38
Sherman Dong <shermand@usc.edu>
2018-03-03 07:53:57 -0800

    revert

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message);
+    session.send("You said: %s", session.message.text);
 });
 
 var http = require('http');
