4908b46a1fadba9991c560a7ba6276ab27d1eb8c
Sherman Dong <shermand@usc.edu>
2018-03-03 04:01:59 -0800

    facebook starter

--- app.js
+++ app.js
@@ -21,3 +21,20 @@ server.post('/api/messages', connector.listen());
 var bot = new builder.UniversalBot(connector, function (session) {
     session.send("You said: %s", session.message.text);
 });
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId            : '1938888989761473',
+    autoLogAppEvents : true,
+    xfbml            : true,
+    version          : 'v2.12'
+  });
+};
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "https://connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
